43031fb26633c71dff95e98ca5f58d67af823ece
codeStackr <rdzaman20000@gmail.com>
2023-07-08 05:37:55 +0600

    fix eslint and styleint errors

--- About/About.js
+++ About/About.js
@@ -1,18 +1,17 @@
 // mobmenu
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("mobmenu").addEventListener("click", function () {
-      document.getElementById("menu").style.display = "block";
-    });
-  
-    document.querySelector("#menu .exit").addEventListener("click", function () {
-      document.getElementById("menu").style.display = "none";
-    });
-  
-    var menuItems = document.querySelectorAll("#menu .menu_item a");
-    menuItems.forEach(function (item) {
-      item.addEventListener("click", function () {
-        document.getElementById("menu").style.display = "none";
-      });
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('mobmenu').addEventListener('click', () => {
+    document.getElementById('menu').style.display = 'block';
+  });
+
+  document.querySelector('#menu .exit').addEventListener('click', () => {
+    document.getElementById('menu').style.display = 'none';
+  });
+
+  const menuItems = document.querySelectorAll('#menu .menu_item a');
+  menuItems.forEach((item) => {
+    item.addEventListener('click', () => {
+      document.getElementById('menu').style.display = 'none';
     });
   });
-  
+});
--- Home/Home_js.js
+++ Home/Home_js.js
@@ -1,38 +1,38 @@
 // mobmenu
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("mobmenu").addEventListener("click", function () {
-    document.getElementById("menu").style.display = "block";
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('mobmenu').addEventListener('click', () => {
+    document.getElementById('menu').style.display = 'block';
   });
 
-  document.querySelector("#menu .exit").addEventListener("click", function () {
-    document.getElementById("menu").style.display = "none";
+  document.querySelector('#menu .exit').addEventListener('click', () => {
+    document.getElementById('menu').style.display = 'none';
   });
 
-  var menuItems = document.querySelectorAll("#menu .menu_item a");
-  menuItems.forEach(function (item) {
-    item.addEventListener("click", function () {
-      document.getElementById("menu").style.display = "none";
+  const menuItems = document.querySelectorAll('#menu .menu_item a');
+  menuItems.forEach((item) => {
+    item.addEventListener('click', () => {
+      document.getElementById('menu').style.display = 'none';
     });
   });
 });
 
 // moreless
 function toggleDiv() {
-  if (window.matchMedia("(max-width: 768px)").matches) {
-    var div = document.getElementById("hiddenDiv");
-    div.classList.toggle("hidden");
+  if (window.matchMedia('(max-width: 768px)').matches) {
+    const div = document.getElementById('hiddenDiv');
+    div.classList.toggle('hidden');
 
-    var button = document.querySelector(".more");
-    var buttonText = button.firstChild;
-    var icon = button.querySelector(".icon");
+    const button = document.querySelector('.more');
+    const buttonText = button.firstChild;
+    const icon = button.querySelector('.icon');
 
-    if (buttonText.nodeValue === "More") {
-      buttonText.nodeValue = "Less";
-      icon.src = "/images/up.png";
+    if (buttonText.nodeValue === 'More') {
+      buttonText.nodeValue = 'Less';
+      icon.src = '/images/up.png';
     } else {
-      buttonText.nodeValue = "More";
-      icon.src = "/images/down.png";
+      buttonText.nodeValue = 'More';
+      icon.src = '/images/down.png';
     }
   }
 }
-
+toggleDiv();
