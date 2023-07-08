31c883512f8a8efa42f0d808c127d6aa560fa0b2
codeStackr <rdzaman20000@gmail.com>
2023-07-08 12:19:35 +0600

    Fix button

--- Home/Home_js.js
+++ Home/Home_js.js
@@ -1,8 +1,24 @@
+
+
 // mobmenu
 document.addEventListener('DOMContentLoaded', () => {
-  if (window.matchMedia('(min-width: 769px)').matches) {
-    document.getElementById('Jp').textContent = 'SEE THE WHOLE PROGRAM';
+  const button = document.getElementById('Jp');
+
+  function handleButton() {
+    if (window.matchMedia('(max-width: 768px)').matches) {
+      button.textContent = 'Join Gamers Summit 2015';
+    } else {
+      button.textContent = 'SEE THE WHOLE PROGRAM';
+    }
   }
+
+  // Initial check on page load
+  handleButton();
+
+  // Add event listener for window resize
+  window.addEventListener('resize', handleButton);
+
+  
   document.getElementById('mobmenu').addEventListener('click', () => {
     document.getElementById('menu').style.display = 'block';
   });
@@ -45,13 +61,13 @@ document.addEventListener('DOMContentLoaded', () => {
     {
       name: 'Ubisoft',
       imageSrc: '/images/ubisoft.png',
-      description: 'A prominent French video game company known for developing, publishing, and distributing a wide range of popular video games. The company was founded in 1986 by the Guillemot family and has since become one of the largest and most influential players in the gaming industry.',
+      description: 'Ubisoft is a video game developer and publisher',
       additionalDescription: 'Ubisoft has studios located worldwide, including in Montreal, Toronto, Paris, Milan, and Shanghai.',
     },
     {
       name: 'Intel',
       imageSrc: '/images/intel.png',
-      description: 'Intel is a multinational technology company renowned for designing and manufacturing a wide range of semiconductor chips, processors, and other hardware components for computers and various electronic devices.',
+      description: 'Intel is a semiconductor company',
       additionalDescription: "Intel's influence extends beyond the consumer and enterprise computing markets.",
     },
   ];
@@ -96,13 +112,13 @@ document.addEventListener('DOMContentLoaded', () => {
     {
       name: 'Nvidia',
       imageSrc: '/images/nvidia.png',
-      description: 'A leading technology company that specializes in designing and manufacturing graphics processing units (GPUs), system-on-a-chip units (SoCs), and other related hardware and software solutions.',
+      description: 'Nvidia is a graphics processing unit (GPU) manufacturer',
       additionalDescription: "NVIDIA's innovations and technologies have made a significant impact on the gaming industry, scientific research, AI development, and other computationally intensive fields.",
     },
     {
       name: 'Logitech',
       imageSrc: '/images/logitech.png',
-      description: 'Logitech is a Swiss company that specializes in computer peripherals and accessories. It was founded in 1981 and has since become one of the leading manufacturers of keyboards, mice, webcams, speakers, and other computer accessories.',
+      description: 'Logitech is a computer peripheral and accessories company',
       additionalDescription: 'Logitech is a well-respected brand in the computer peripherals industry, providing a wide range of products that cater to different needs and preferences.',
     },
   ];
